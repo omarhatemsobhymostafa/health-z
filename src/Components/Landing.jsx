1334f1472e63660d582a97670bc3f50983595846
= <omar.hatem.okasha@gmail.com>
2025-06-21 20:58:10 +0300

    image landinhg edit

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -5,11 +5,11 @@ import img from './../images/illust1.png'
 export default function Landing() {
   return (
     <>
-      <div className='h-[100vh] w-full bg-[rgb(120,216,222)] flex flex-col items-center py-32'>
-            <img src={img} alt=""  className='w-1/4 h-96 ' />
+      <div className='h-[100vh] w-full bg-[rgb(120,216,222)] flex flex-col items-center py-32 px-6'>
+            <img src={img} alt=""  className='h-1/2' />
             <h2 className='text-3xl mt-7'>  خليكي رئيسة مجلس ادارة صحتك</h2>
             <p className='mt-4 text-lg'>ابني عاداتك الصحية</p>
-            <Link to='/dashboard'><button className='bg-[#0a5d89] w-96 h-12 mt-5 text-white rounded-md text-xl'>ابدأي دلوقتي</button></Link>
+            <Link to='/dashboard'><button className='bg-[#0a5d89] w-80 h-12 mt-5 text-white rounded-md text-xl '>ابدأي دلوقتي</button></Link>
             <div className='flex gap-2 mt-1'><span>اصلا عندي حساب؟</span><Link to={'./login'} className='underline text-green-600'>تسجيل الدخول</Link></div>
       </div> 
     </>
