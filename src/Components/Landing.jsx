a4d61468bdbb172715c3f901eb9c42ebfd143d15
= <omar.hatem.okasha@gmail.com>
2025-06-22 20:07:41 +0300

    edit in lanidng page in word

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -7,7 +7,7 @@ export default function Landing() {
     <>
       <div className='h-[100vh] w-full bg-[rgb(120,216,222)] flex flex-col items-center py-32 px-6'>
             <img src={img} alt=""  className='h-1/2' />
-            <h2 className='text-3xl mt-7'>  خليكي رئيسة مجلس ادارة صحتك</h2>
+            <h2 className='text-3xl mt-7'>      خليكي مديرة صحتك</h2>
             <p className='mt-4 text-lg'>ابني عاداتك الصحية</p>
             <Link to='/dashboard'><button className='bg-[#0a5d89] w-80 h-12 mt-5 text-white rounded-md text-xl '>ابدأي دلوقتي</button></Link>
             <div className='flex gap-2 mt-1'><span>اصلا عندي حساب؟</span><Link to={'./login'} className='underline text-green-600'>تسجيل الدخول</Link></div>
