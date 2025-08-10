437d5aa8cede865db0be71acc770e7ef4d37f54a
omarhatemsobhymostafa <omar.hatem.okasha@gmail.com>
2025-08-10 19:53:27 +0300

    change to 3afyty

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
-    <nav className='px-6 md:px-20 py-1 flex flex-col md:flex-row-reverse gap-4 md:gap-0 justify-between items-center fixed w-full bg-[#250c6a] shadow z-50'>
+    <nav className='px-6 md:px-20 py-1 flex flex-col md:flex-row gap-4 md:gap-0 justify-between items-center fixed w-full bg-[#250c6a] shadow z-50'>
       <Link to='/'><img src={Logo} className='h-16 md:h-16 rounded-full ' alt="Logo" /></Link>
 
       <div className="flex gap-3 md:gap-5 flex-row md:flex-row items-center">
